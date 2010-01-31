1205634255988bcabebc8b08e60bee3c25e1ab7d
Michael Snoyman <michael@snoyman.com>
2010-02-01 01:24:34 +0200

    Added documentation

--- Network/Wai.hs
+++ Network/Wai.hs
@@ -1,6 +1,38 @@
 {-# LANGUAGE Rank2Types #-}
+{-|
+
+This module defines a generic web application interface. It is a common
+protocol between web servers and web applications.
+
+The overriding design principles here are performance, generality and type
+safety. To address performance, this library is built on 'Enumerator's for the
+request and response bodies. The advantages of this approach over lazy IO has
+been debated elsewhere.
+
+Nonetheless, many people find 'Enumerator's difficult to work with. For that
+reason, this library includes the "Network.Wai.Enumerator" module to provide
+more familiar abstractions, including lazy IO.
+
+Generality is achieved by removing many variables commonly found in similar
+projects that are not universal to all servers. The goal is that the 'Request'
+object contains only data which is meaningful in all circumstances.
+
+Unlike other approaches, this package declares many data types to assist in
+type safety. This feels more in the general Haskell spirit.
+
+A final note: please remember when using this package that, while your
+application my compile without a hitch against many different servers, there
+are other considerations to be taken when moving to a new backend. For example,
+if you transfer from a CGI application to a FastCGI one, you might suddenly
+find you have a memory leak. Conversely, a FastCGI application would be
+well served to preload all templates from disk when first starting; this
+would kill the performance of a CGI application.
+
+-}
 module Network.Wai
     ( -- * Data types
+
+      -- $show_read
       -- ** Request method
       Method (..)
     , methodFromBS
@@ -35,9 +67,16 @@ module Network.Wai
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B8
 
--- | Please do not use the Show and Read instances for anything other than
--- debugging purposes. Instead, the 'methodFromBS' and 'methodToBS' provide a
--- more appropriate interface.
+-- $show_read
+--
+-- For the data types below, you should only use the 'Show' and 'Read'
+-- instances for debugging purposes. Each datatype (excepting 'UrlScheme') has
+-- associated functions for converting to and from strict 'B.ByteString's;
+-- these are approrpiate for generating content.
+
+-- | HTTP request method. This data type is extensible via the Method
+-- constructor. Request methods are case-sensitive, and comparison is achieved
+-- by converting to a 'B.ByteString' via 'methodToBS'.
 data Method =
     OPTIONS
   | GET
@@ -48,7 +87,10 @@ data Method =
   | TRACE
   | CONNECT
   | Method B.ByteString
-  deriving (Show, Read, Eq)
+  deriving (Show, Read)
+
+instance Eq Method where
+    x == y = methodToBS x == methodToBS y
 
 methodFromBS :: B.ByteString -> Method
 methodFromBS bs
@@ -75,16 +117,25 @@ methodToBS TRACE = B8.pack "TRACE"
 methodToBS CONNECT = B8.pack "CONNECT"
 methodToBS (Method bs) = bs
 
-data UrlScheme = HTTP | HTTPS deriving (Show, Eq)
+data UrlScheme = HTTP | HTTPS
+    deriving (Show, Eq)
 
--- | The value of HttpVersion should be the information trailing HTTP/.
+-- | Version of HTTP protocol used in current request. This data type is
+-- extensible via the HttpVersion constructor. Comparison is achieved by
+-- converting to a 'B.ByteString' via 'httpVersionToBS'.
 data HttpVersion =
       Http09
     | Http10
     | Http11
     | HttpVersion B.ByteString
-    deriving (Show, Eq)
+    deriving (Show, Read)
 
+instance Eq HttpVersion where
+    x == y = httpVersionToBS x == httpVersionToBS y
+
+-- | This function takes the information after \"HTTP\/\". For example:
+--
+-- @ 'httpVersionFromBS' ('B8.pack' \"1.0\") == 'Http10' @
 httpVersionFromBS :: B.ByteString -> HttpVersion
 httpVersionFromBS bs
     | B.length bs == 3 = case B8.unpack bs of
@@ -94,12 +145,25 @@ httpVersionFromBS bs
         _ -> HttpVersion bs
     | otherwise = HttpVersion bs
 
+-- | Returns the version number, for example:
+--
+-- @ 'B8.unpack' ('httpVersionToBS' 'Http10') == \"1.0\" @
 httpVersionToBS :: HttpVersion -> B.ByteString
 httpVersionToBS Http09 = B8.pack "0.9"
 httpVersionToBS Http10 = B8.pack "1.0"
 httpVersionToBS Http11 = B8.pack "1.1"
 httpVersionToBS (HttpVersion bs) = bs
 
+-- | Headers sent from the client to the server. Clearly, this is not a
+-- complete list of all possible headers, but rather a selection of common
+-- ones. If other headers are required, they can be created with the
+-- RequestHeader constructor.
+--
+-- The naming rules are simple: removing any hyphens from the actual name, and
+-- if there is a naming conflict with a 'ResponseHeader', prefix with Req.
+--
+-- Equality determined by conversion via 'requestHeaderToBS'. Remember, headers
+-- are case sensitive.
 data RequestHeader =
       Accept
     | AcceptCharset
@@ -112,7 +176,7 @@ data RequestHeader =
     | Host
     | Referer
     | RequestHeader B.ByteString
-    deriving (Show)
+    deriving (Show, Read)
 
 instance Eq RequestHeader where
     x == y = requestHeaderToBS x == requestHeaderToBS y
@@ -144,6 +208,16 @@ requestHeaderToBS Host = B8.pack "Host"
 requestHeaderToBS Referer = B8.pack "Referer"
 requestHeaderToBS (RequestHeader bs) = bs
 
+
+-- | Headers sent from the server to the client. Clearly, this is not a
+-- complete list of all possible headers, but rather a selection of common
+-- ones. If other headers are required, they can be created with the
+-- ResponseHeader constructor.
+--
+-- if there is a naming conflict with a 'ResponseHeader', prefix with Req.
+--
+-- Equality determined by conversion via 'responseHeaderToBS'. Remember,
+-- headers are case sensitive.
 data ResponseHeader =
       ContentEncoding
     | ContentLanguage
@@ -159,7 +233,6 @@ data ResponseHeader =
 
 instance Eq ResponseHeader where
     x == y = responseHeaderToBS x == responseHeaderToBS y
-
 
 responseHeaderFromBS :: B.ByteString -> ResponseHeader
 responseHeaderFromBS bs = case B8.unpack bs of
@@ -187,7 +260,7 @@ responseHeaderToBS SetCookie = B8.pack "Set-Cookie"
 responseHeaderToBS (ResponseHeader bs) = bs
 
 -- | This attempts to provide the most common HTTP status codes, not all of
--- them. Use the 'Status' constructor when you want to create a status code not
+-- them. Use the Status constructor when you want to create a status code not
 -- provided.
 --
 -- The 'Eq' instance tests equality based only on the numeric status code
@@ -235,13 +308,47 @@ statusMessage Status405 = B8.pack "Method Not Allowed"
 statusMessage Status500 = B8.pack "Internal Server Error"
 statusMessage (Status _ m) = m
 
+-- | An enumerator is a data producer. It takes two arguments: a function to
+-- enumerate over (the iteratee) and an accumulating parameter. As the
+-- enumerator produces output, it calls the iteratee, thereby avoiding the need
+-- to allocate large amounts of memory for storing the entire piece of data.
+--
+-- Normally in Haskell, we can achieve the same results with laziness. For
+-- example, an inifinite list does not require inifinite memory storage; we
+-- simply get away with thunks. However, when operating in the IO monad, we do
+-- not have this luxury. There are other approaches, such as lazy I\/O. If you
+-- would like to program in this manner, please see
+-- "Network.Wai.Enumerator", in particular toLBS.
+--
+-- That said, let's address the details of this particular enumerator
+-- implementation. You'll notice that the iteratee is a function that takes two
+-- arguments and returns an 'Either' value. The second argument is simply the
+-- piece of data generated by the enumerator. The 'Either' value at the end is
+-- a means to alert the enumerator whether to continue or not. If it returns
+-- 'Left', then the enumeration should cease. If it returns 'Right', it should
+-- continue.
+--
+-- The accumulating parameter (a) has meaning only to the iteratee; the
+-- enumerator simply passes it around. The enumerator itself also returns an
+-- 'Either' value; a 'Right' means the enumerator ran to completion, while a
+-- 'Left' indicates early termination was requested by the iteratee.
+--
+-- 'Enumerator's are used twice in the WAI: once in the 'Request' datatype to
+-- access the request body, and once in the 'Response' datatype for the
+-- application to return the body of the response.
 type Enumerator a = (a -> B.ByteString -> IO (Either a a))
                  -> a
                  -> IO (Either a a)
 
+-- | Information on the request sent by the client. This abstracts away the
+-- details of the underlying implementation.
 data Request = Request
   {  requestMethod  :: Method
   ,  httpVersion    :: HttpVersion
+  -- | Extra path information sent by the client. The meaning varies slightly
+  -- depending on backend; in a standalone server setting, this is most likely
+  -- all information after the domain name. In a CGI application, this would be
+  -- the information following the path to the CGI executable itself.
   ,  pathInfo       :: B.ByteString
   ,  queryString    :: B.ByteString
   ,  serverName     :: B.ByteString
@@ -250,15 +357,31 @@ data Request = Request
   ,  urlScheme      :: UrlScheme
   ,  requestBody    :: forall a. Enumerator a
   ,  errorHandler   :: String -> IO ()
+  -- | The client\'s host information.
   ,  remoteHost     :: B.ByteString
   }
 
 data Response = Response
   { status        :: Status
   , headers       :: [(ResponseHeader, B.ByteString)]
+  -- | A common optimization is to use the sendfile system call when sending
+  -- files from the disk. This datatype facilitates this optimization; if
+  -- 'Left' is returns, the server will send the file from the disk by whatever
+  -- means it wishes. If 'Right', it will call the 'Enumerator'.
   , body          :: forall a. Either FilePath (Enumerator a)
   }
 
 type Application = Request -> IO Response
 
+-- | Middleware is a component that sits between the server and application. It can do such tasks as GZIP encoding or response caching. What follows is the general definition of middleware, though a middleware author should feel free to modify this.
+--
+-- As an example of an alternate type for middleware, suppose you write a
+-- function to load up session information. The session information is simply a
+-- string map \[(String, String)\]. A logical type signatures for this middleware
+-- might be:
+--
+-- @ loadSession :: ([(String, String)] -> Application) -> Application @
+--
+-- Here, instead of taking a standard 'Application' as its first argument, the
+-- middleware takes a function which consumes the session information as well.
 type Middleware = Application -> Application
--- Network/Wai/Enumerator.hs
+++ Network/Wai/Enumerator.hs
@@ -22,6 +22,7 @@ import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString as B
 import System.IO.Unsafe (unsafeInterleaveIO)
 import System.IO (withBinaryFile, IOMode (ReadMode), Handle, hIsEOF)
+import Data.ByteString.Lazy.Internal (defaultChunkSize)
 
 -- | Performs a specified conversion on each 'B.ByteString' output by an
 -- enumerator.
@@ -41,6 +42,7 @@ toLBS e = L.fromChunks `fmap` helper where
                         xs <- helper
                         return $ x' : xs
 
+-- | This function safely converts a lazy bytestring into an enumerator.
 fromLBS :: L.ByteString -> (forall a. Enumerator a)
 fromLBS lbs iter a0 = helper a0 $ L.toChunks lbs where
     helper a [] = return $ Right a
@@ -50,6 +52,9 @@ fromLBS lbs iter a0 = helper a0 $ L.toChunks lbs where
             Left a' -> return $ Left a'
             Right a' -> helper a' xs
 
+-- | Same as 'fromLBS', but the lazy bytestring is in the IO monad. This allows
+-- you to lazily read a file into memory, perform some mapping on the data and
+-- convert it into an enumerator.
 fromLBS' :: IO L.ByteString -> (forall a. Enumerator a)
 fromLBS' lbs' iter a0 = lbs' >>= \lbs -> fromLBS lbs iter a0
 
@@ -59,24 +64,27 @@ fromLBS' lbs' iter a0 = lbs' >>= \lbs -> fromLBS lbs iter a0
 toSource :: Enumerator (Maybe B.ByteString) -> IO (Maybe B.ByteString)
 toSource e = fmap (either id id) $ e (const $ return . Left . Just) Nothing
 
-chunkSize :: Int
-chunkSize = 1024 -- FIXME
-
+-- | Read a chunk of data from the given 'Handle' at a time. We use
+-- 'defaultChunkSize' from the bytestring package to determine the largest
+-- chunk to take.
 fromHandle :: Handle -> Enumerator a
 fromHandle h iter a = do
     eof <- hIsEOF h
     if eof
         then return $ Right a
         else do
-            bs <- B.hGet h chunkSize
+            bs <- B.hGet h defaultChunkSize
             ea' <- iter a bs
             case ea' of
                 Left a' -> return $ Left a'
                 Right a' -> fromHandle h iter a'
 
+-- | A little wrapper around 'fromHandle' which first opens a file for reading.
 fromFile :: FilePath -> Enumerator a
 fromFile fp iter a0 = withBinaryFile fp ReadMode $ \h -> fromHandle h iter a0
 
+-- | Since the response body is defined as an 'Either' 'FilePath' 'Enumerator',
+-- this function simply reduces the whole operator to an enumerator. This can
+-- be convenient for server implementations not optimizing file sending.
 fromEitherFile :: Either FilePath (Enumerator a) -> Enumerator a
-fromEitherFile (Left fp) = fromFile fp
-fromEitherFile (Right e) = e
+fromEitherFile = either fromFile id
