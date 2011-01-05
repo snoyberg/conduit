41f2b8213e95537a6fb950e74c7d727cf2f3899d
Michael <michael@snoyman.com>
2011-01-06 00:21:20 +0200

    Added mimeTypes to Util

--- Network/Wai/Handler/Warp/Util.hs
+++ Network/Wai/Handler/Warp/Util.hs
@@ -1,8 +1,15 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Network.Wai.Handler.Warp.Util
     ( getFileSize
+    , mimeTypes
     ) where
 
+import UtilHelper (mimeTypes')
+import qualified Data.Map as Map
+import Data.ByteString (ByteString)
+import Language.Haskell.TH.Syntax (qRunIO, lift)
+import qualified Data.ByteString.Char8 as S8
 
 #if defined(mingw32_HOST_OS) || defined(__MINGW32__)
 
@@ -24,3 +31,9 @@ getFileSize :: FilePath -> IO Integer
 getFileSize = fmap (fromIntegral . fileSize) . getFileStatus
 
 #endif
+
+mimeTypes :: Map.Map ByteString ByteString
+mimeTypes =
+    Map.fromAscList $ map go $(qRunIO mimeTypes' >>= lift)
+  where
+    go (x, y) = (S8.pack x, S8.pack y)
---
+++ UtilHelper.hs
@@ -0,0 +1,16 @@
+module UtilHelper where
+
+import Data.Maybe (mapMaybe)
+import Data.List (sortBy)
+import Data.Ord (comparing)
+
+mimeTypes' :: IO [(String, String)]
+mimeTypes' = do
+    s <- readFile "mime.types"
+    let go ('#':_) = Nothing
+        go s =
+            case words s of
+                [a, b] -> Just (b, a)
+                _ -> Nothing
+    let pairs = mapMaybe go $ lines s
+    return $ sortBy (comparing fst) pairs
