49be5a707ff8facbd58db437d8426a19968c3858
Michael Snoyman <michael@snoyman.com>
2011-01-11 20:00:51 +0200

    Including test program + lighttpd config file

---
+++ test.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Network.Wai
+import Network.Wai.Handler.SCGI
+import Control.Monad.IO.Class
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.Enumerator as E
+
+main = run app
+
+app req
+    | pathInfo req == "/" = return $ responseLBS status200 [("Content-Type", "text/html")] "<form method='post' action='/post'><input type='text' name='name'><input type='submit'></form>"
+app req = do
+    bss <- E.consume
+    return $ responseLBS status200 [("Content-Type", "text/plain")] $ L.concat
+        [ L8.pack $ show $ requestHeaders req
+        , "\n"
+        , L.fromChunks bss
+        ]
