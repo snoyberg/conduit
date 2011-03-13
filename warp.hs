97ded724628bce03f1b0d19475a091e5b45191de
Michael Snoyman <michael@snoyman.com>
2011-03-13 08:38:43 +0200

    canonicalizePath

--- warp.hs
+++ warp.hs
@@ -6,22 +6,26 @@ import Network.Wai.Handler.Warp (run)
 import System.Environment (getArgs)
 import System.Console.CmdArgs
 import Text.Printf (printf)
+import System.Directory (canonicalizePath)
+import Control.Monad (unless)
 
 data Args = Args
     { docroot :: FilePath
     , index :: [FilePath]
     , port :: Int
     , noindex :: Bool
+    , quiet :: Bool
     }
     deriving (Show, Data, Typeable)
 
-defaultArgs = Args "." ["index.html", "index.htm"] 3000 False
+defaultArgs = Args "." ["index.html", "index.htm"] 3000 False False
 
 main :: IO ()
 main = do
     Args {..} <- cmdArgs defaultArgs
+    docroot' <- canonicalizePath docroot
     args <- getArgs
-    printf "Serving directory %s on port %d with %s index files.\n" docroot port (if noindex then "no" else show index)
+    unless quiet $ printf "Serving directory %s on port %d with %s index files.\n" docroot' port (if noindex then "no" else show index)
     run port $ staticApp StaticSettings
         { ssFolder = docroot
         , ssIndices = if noindex then [] else index
