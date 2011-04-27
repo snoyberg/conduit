1d07e82154df3d350438227f0bebc9c478091005
Michael <michael@snoyman.com>
2011-04-27 09:21:30 +0300

    Fix test program

--- wai-handler-scgi/test.hs
+++ wai-handler-scgi/test.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Network.Wai
-import Network.Wai.Handler.SCGI
-import Control.Monad.IO.Class
+import Network.Wai.Handler.SCGI (run)
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as L8
-import qualified Data.Enumerator as E
+import qualified Data.Enumerator.List as E
+import Network.HTTP.Types (status200)
 
 main = run app
 
 app req
-    | pathInfo req == "/" = return $ responseLBS status200 [("Content-Type", "text/html")] "<form method='post' action='/post'><input type='text' name='name'><input type='submit'></form>"
+    | rawPathInfo req == "/" = return $ responseLBS status200 [("Content-Type", "text/html")] "<form method='post' action='/post'><input type='text' name='name'><input type='submit'></form>"
 app req = do
     bss <- E.consume
     return $ responseLBS status200 [("Content-Type", "text/plain")] $ L.concat
