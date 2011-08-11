533c93d34ec2ebfc1f16c02812eed1dd562730cd
Greg Weber <greg@gregweber.info>
2011-08-11 09:46:55 -0700

    install dependencies in ./install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
+CABAL=cabal
 # Note: Ignoring wai-handler-devel, wai-handler-webkit
 PACKAGES="wai wai-test wai-extra wai-app-static wai-handler-fastcgi wai-handler-launch wai-handler-scgi warp warp-static"
-CABAL=cabal
-
 for package in $PACKAGES
 do
     echo Installing $package
     cd $package
     ($CABAL configure --enable-tests ||
-      ($CABAL install --only-dependencies && $CABAL configure --enable-tests)
+      ($CABAL update && $CABAL install HUnit QuickCheck hspec && $CABAL install --only-dependencies && $CABAL configure --enable-tests)
     ) && $CABAL build && $CABAL test && ./Setup.lhs install || exit
     cd ..
 done
