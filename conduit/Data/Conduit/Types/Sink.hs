0bc263040f612a5482fe819e4763c928bac8db47
Michael Snoyman <michael@snoyman.com>
2012-01-27 10:31:33 +0200

    Simpler Applicative instance

--- conduit/Data/Conduit/Types/Sink.hs
+++ conduit/Data/Conduit/Types/Sink.hs
@@ -13,7 +13,7 @@ module Data.Conduit.Types.Sink
 import Control.Monad.Trans.Resource
 import Control.Monad.Trans.Class (MonadTrans (lift))
 import Control.Monad.IO.Class (MonadIO (liftIO))
-import Control.Monad (liftM)
+import Control.Monad (liftM, ap)
 import Control.Applicative (Applicative (..))
 import Control.Monad.Base (MonadBase (liftBase))
 
@@ -85,74 +85,11 @@ instance Monad m => Functor (Sink input m) where
     fmap f (Sink msink) = Sink (liftM (fmap f) msink)
 
 instance Resource m => Applicative (Sink input m) where
-    pure x = Sink (return (SinkNoData x))
-    Sink mf <*> Sink ma = Sink $ do
-        f <- mf
-        a <- ma
-        case (f, a) of
-            (SinkNoData f', SinkNoData a') -> return (SinkNoData (f' a'))
-            _ -> do
-                istate <- newRef (toEither f, toEither a)
-                return $ appHelper istate
-
-toEither :: PreparedSink input m output -> SinkEither input m output
-toEither (SinkData x y) = SinkPair x y
-toEither (SinkNoData x) = SinkOutput x
-
-type SinkPush input m output = input -> ResourceT m (SinkResult input output)
-type SinkClose input m output = ResourceT m output
-data SinkEither input m output
-    = SinkPair (SinkPush input m output) (SinkClose input m output)
-    | SinkOutput output
-type SinkState input m a b = Ref (Base m) (SinkEither input m (a -> b), SinkEither input m a)
-
-appHelper :: Resource m => SinkState input m a b -> PreparedSink input m b
-appHelper istate = SinkData (pushHelper istate) (closeHelper istate)
-
-pushHelper :: Resource m
-           => SinkState input m a b
-           -> input
-           -> ResourceT m (SinkResult input b)
-pushHelper istate stream0 = do
-    state <- readRef istate
-    go state stream0
-  where
-    go (SinkPair f _, eb) stream = do
-        mres <- f stream
-        case mres of
-            Processing -> return Processing
-            Done leftover res -> do
-                let state' = (SinkOutput res, eb)
-                writeRef istate state'
-                maybe (return Processing) (go state') leftover
-    go (f@SinkOutput{}, SinkPair b _) stream = do
-        mres <- b stream
-        case mres of
-            Processing -> return Processing
-            Done leftover res -> do
-                let state' = (f, SinkOutput res)
-                writeRef istate state'
-                maybe (return Processing) (go state') leftover
-    go (SinkOutput f, SinkOutput b) leftover = return $ Done (Just leftover) $ f b
-
-closeHelper :: Resource m
-            => SinkState input m a b
-            -> ResourceT m b
-closeHelper istate = do
-    (sf, sa) <- readRef istate
-    case sf of
-        SinkOutput f -> go' f sa
-        SinkPair _ close -> do
-            f <- close
-            go' f sa
-  where
-    go' f (SinkPair _ close) = do
-        a <- close
-        return (f a)
-    go' f (SinkOutput a) = return (f a)
+    pure = return
+    (<*>) = ap
 
 instance Resource m => Monad (Sink input m) where
-    return = pure
+    return x = Sink (return (SinkNoData x))
     mx >>= f = Sink $ do
         x <- prepareSink mx
         case x of
