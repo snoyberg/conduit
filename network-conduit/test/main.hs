d2ad4a4c811400feb864f36885ad7c612927851e
Michael Snoyman <michael@snoyman.com>
2012-02-01 18:38:49 +0200

    Added socket leak test

---
+++ network-conduit/test/main.hs
@@ -0,0 +1,17 @@
+import Data.Conduit
+import Data.Conduit.Network
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (replicateM_)
+
+main :: IO ()
+main = do
+    _ <- forkIO $ runTCPServer (ServerSettings 4000 Nothing) echo
+    threadDelay 1000
+    replicateM_ 10000
+        $ runTCPClient (ClientSettings 4000 "localhost") doNothing
+
+echo :: Application
+echo src sink = src $$ sink
+
+doNothing :: Application
+doNothing _ _ = return ()
