0528a8ddccd74adae4d373537339ec9a91e67435
Myles C. Maxfield <myles.maxfield@gmail.com>
2012-02-07 10:50:22 -0800

    Throws a GetError on premature close, and works with empty 'pull' requests

--- Data/Conduit/Cereal.hs
+++ Data/Conduit/Cereal.hs
@@ -11,29 +11,34 @@ import qualified Data.Conduit as DC
 import Data.Conduit.List (sourceList)
 import Data.Serialize.Get
 import Data.Serialize.Put
+import Control.Exception (throw)
 
-data GetError = GetError String
-              | PrematureClose
+data GetException = GetException String
+                  | GetDoesntConsumeInput
   deriving (Show, Typeable)
 
-instance Exception GetError
+instance Exception GetException
 
 -- | Convert a 'Get' into a 'Sink'. The 'Get' will be streamed bytes until it returns 'Done' or 'Fail'.
 --
--- If the 'Get' fails, a GetError will be thrown with 'resourceThrow'
+-- If the 'Get' fails, a GetException will be thrown with 'resourceThrow'. This function itself can also throw a GetException.
 sinkGet :: DC.ResourceThrow m => Get output -> DC.Sink BS.ByteString m output
-sinkGet get = DC.SinkData { DC.sinkPush = push (runGetPartial get)
-                          , DC.sinkClose = close
-                          }
-  where push f input = do
-          case f input of
-            Fail s -> lift $ DC.resourceThrow $ GetError s
-            Partial f' -> return $ DC.Processing (push f') close
-            Done r rest -> return $ DC.Done (if BS.null rest
-                                               then Nothing
-                                               else Just rest
-                                            ) r
-        close = lift $ DC.resourceThrow PrematureClose
+sinkGet get = case runGetPartial get BS.empty of
+                Fail s -> throw $ GetException s
+                Partial f -> DC.SinkData { DC.sinkPush = push f
+                                         , DC.sinkClose = close f
+                                         }
+                Done _ _ -> throw GetDoesntConsumeInput
+  where push f input
+          | BS.null input = return $ DC.Processing (push f) (close f)
+          | otherwise = case f input of
+              Fail s -> lift $ DC.resourceThrow $ GetException s
+              Partial f' -> return $ DC.Processing (push f') (close f')
+              Done r rest -> return $ DC.Done (if BS.null rest
+                                                 then Nothing
+                                                 else Just rest
+                                              ) r
+        close f = let Fail s = f BS.empty in lift $ DC.resourceThrow $ GetException s
 
 -- | Convert a 'Put' into a 'Source'. Runs in constant memory.
 sourcePut :: DC.Resource m => Put -> DC.Source m BS.ByteString
