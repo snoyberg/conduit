bcc2c3180fc05d5bcb6ea9ff0d2f41b9d3c73981
Litherum <litherum@gmail.com>
2012-04-11 23:14:13 -0700

    Allowing for Gets that don't consume any input, and adding a failure mode

--- Data/Conduit/Cereal.hs
+++ Data/Conduit/Cereal.hs
@@ -1,26 +1,38 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+--{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 -- | Turn a 'Get' into a 'Sink' and a 'Put' into a 'Source'
 
-module Data.Conduit.Cereal (sinkGet, conduitGet, sourcePut) where
+module Data.Conduit.Cereal (GetError, sinkGet, conduitGet, sourcePut) where
 
+--import           Control.Exception
+import           Control.Monad.Error
+import           Control.Monad.Error.Class
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Conduit as C
 import           Data.Conduit.List (sourceList)
 import           Data.Serialize hiding (get, put)
-import           Data.Typeable
-import           Control.Exception
+--import           Data.Typeable
 
+{-
 data GetException = GetException String
      deriving (Show, Typeable)
 
 instance Exception GetException
+-}
+
+data GetError = GetError String
+  deriving (Show, Eq)
+
+instance Error GetError where
+  noMsg = GetError ""
+  strMsg = GetError
 
 -- | Convert a 'Get' into a 'Sink'. The 'Get' will be streamed bytes until it returns 'Done' or 'Fail'.
 --
 -- If 'Get' succeed it will return the data read and unconsumed part of the input stream.
--- If the 'Get' fails it will return message describing the error. 
+-- If the 'Get' fails it will return message describing the error.
 sinkGet :: Monad m => Get output -> C.Sink BS.ByteString m (Either String output)
 sinkGet get = consume (runGetPartial get) BS.empty
   where push f input
@@ -33,16 +45,20 @@ sinkGet get = consume (runGetPartial get) BS.empty
         close f = let Fail r = f BS.empty in C.Done Nothing (Left r)
 
 -- | Run a 'Get' repeatedly on the input stream, producing an output stream of whatever the 'Get' outputs.
-conduitGet :: Monad m => Get output -> C.Conduit BS.ByteString m output
-conduitGet get = consume (runGetPartial get) BS.empty BS.empty
+conduitGet :: MonadError GetError m => Get output -> C.Conduit BS.ByteString m output
+conduitGet get = consume True (runGetPartial get) [] BS.empty
   where push f b input
-          | BS.null input = C.NeedInput (push f b) (C.Done (streamToMaybe b) ())
-          | otherwise = consume f b input
-        consume f b s = case f s of
-          Fail msg -> throw $ GetException msg
-          Partial f' -> C.NeedInput (push f' consumed) $ C.Done (streamToMaybe consumed) ()
-          Done r s' -> C.HaveOutput (push (runGetPartial get) BS.empty s') (return ()) r
-          where consumed = BS.concat [b, s]
+          | BS.null input = C.NeedInput (push f b) (C.Done (streamToMaybe $ BS.concat $ reverse b) ())
+          | otherwise = consume False f b input
+        consume initial f b s = case f s of
+          Fail msg -> C.PipeM (throwError $ strMsg msg) undefined
+          Partial f' -> C.NeedInput (push f' consumed) $ C.Done (streamToMaybe $ BS.concat $ reverse consumed) ()
+          Done r s' -> case initial of
+                         True -> infiniteSequence r
+                         False -> C.HaveOutput (push (runGetPartial get) [] s') (return ()) r
+          where consumed = s : b
+                infiniteSequence r = C.HaveOutput (infiniteSequence r) (return ()) r
+                -- infinteSequence only works because the Get will either _always_ consume no input, or _never_ consume no input.
 
 streamToMaybe :: BS.ByteString -> Maybe BS.ByteString
 streamToMaybe s = if BS.null s
--- Test/CerealConduit.hs
+++ Test/CerealConduit.hs
@@ -1,4 +1,4 @@
-module Test.CerealConduit (tests) where
+module Test.CerealConduit where
 
 import Control.Monad.Identity
 import Test.HUnit
@@ -7,9 +7,11 @@ import Data.Conduit.Cereal
 import Data.Conduit.List
 import Data.Serialize
 import qualified Data.ByteString as BS
-import Test.Framework.Providers.HUnit
+--import Test.Framework.Providers.HUnit
 import System.Exit
 import Data.Word
+import qualified Data.List as L
+import Prelude hiding (take)
 
 sinktest1 :: Test
 sinktest1 = TestCase (assertEqual "Handles starting with empty bytestring"
@@ -35,6 +37,11 @@ sinktest4 = TestCase (assertEqual "Consumes no data"
   (Right ())
   (runIdentity $ (sourceList [BS.pack [1]]) C.$$ (sinkGet $ return ())))
 
+sinktest5 :: Test
+sinktest5 = TestCase (assertEqual "Empty list"
+  (Right ())
+  (runIdentity $ (sourceList []) C.$$ (sinkGet $ return ())))
+
 twoItemGet :: Get Word8
 twoItemGet = do
   x <- getWord8
@@ -43,41 +50,91 @@ twoItemGet = do
 
 conduittest1 :: Test
 conduittest1 = TestCase (assertEqual "Handles starting with empty bytestring"
-  []
-  (runIdentity $ (sourceList [BS.pack [], BS.pack [1]]) C.$= conduitGet twoItemGet C.$$ consume))
+  (Right [])
+  ((sourceList [BS.pack [], BS.pack [1]]) C.$= conduitGet twoItemGet C.$$ consume))
 
 conduittest2 :: Test
 conduittest2 = TestCase (assertEqual "Works when the get is split across items"
-  [3]
-  (runIdentity $ (sourceList [BS.pack [1], BS.pack [2]]) C.$= conduitGet twoItemGet C.$$ consume))
+  (Right [3])
+  ((sourceList [BS.pack [1], BS.pack [2]]) C.$= conduitGet twoItemGet C.$$ consume))
 
 conduittest3 :: Test
 conduittest3 = TestCase (assertEqual "Works when empty bytestring in middle of get"
-  [3]
-  (runIdentity $ (sourceList [BS.pack [1], BS.pack [], BS.pack [2]]) C.$= conduitGet twoItemGet C.$$ consume))
+  (Right [3])
+  ((sourceList [BS.pack [1], BS.pack [], BS.pack [2]]) C.$= conduitGet twoItemGet C.$$ consume))
 
 conduittest4 :: Test
 conduittest4 = TestCase (assertEqual "Works when empty bytestring at end of get"
-  [3, 7]
-  (runIdentity $ (sourceList [BS.pack [1, 2], BS.pack [], BS.pack [3, 4]]) C.$= conduitGet twoItemGet C.$$ consume))
+  (Right [3])
+  ((sourceList [BS.pack [1, 2], BS.pack []]) C.$= conduitGet twoItemGet C.$$ consume))
 
 conduittest5 :: Test
 conduittest5 = TestCase (assertEqual "Works when multiple gets are in an item"
-  [3, 7]
-  (runIdentity $ (sourceList [BS.pack [1, 2, 3, 4]]) C.$= conduitGet twoItemGet C.$$ consume))
+  (Right [3, 7])
+  ((sourceList [BS.pack [1, 2, 3, 4]]) C.$= conduitGet twoItemGet C.$$ consume))
 
 conduittest6 :: Test
 conduittest6 = TestCase (assertEqual "Works with leftovers"
-  [3]
-  (runIdentity $ (sourceList [BS.pack [1, 2, 3]]) C.$= conduitGet twoItemGet C.$$ consume))
+  (Right [3])
+  ((sourceList [BS.pack [1, 2, 3]]) C.$= conduitGet twoItemGet C.$$ consume))
 
-sinktests = TestList [sinktest1, sinktest2, sinktest3, sinktest4]
+conduittest7 :: Test
+conduittest7 = let c = 10 in TestCase (assertEqual "Works with infinite lists"
+  (Right $ L.replicate c ())
+  ((sourceList [BS.pack [1, 2, 3]]) C.$= conduitGet (return ()) C.$$ take c))
 
-conduittests = TestList [conduittest1, conduittest2, conduittest3, conduittest4, conduittest5, conduittest6]
+conduittest8 :: Test
+conduittest8 = let c = 10 in TestCase (assertEqual "Works with empty source and infinite lists"
+  (Right $ L.replicate c ())
+  ((sourceList []) C.$= conduitGet (return ()) C.$$ take c))
+
+conduittest9 :: Test
+conduittest9 = let c = 10 in TestCase (assertEqual "Works with two well-placed items"
+  (Right [3, 7])
+  ((sourceList [BS.pack [1, 2], BS.pack [3, 4]]) C.$= conduitGet twoItemGet C.$$ consume))
+
+conduittest10 :: Test
+conduittest10 = TestCase (assertBool "Failure works"
+  (case (sourceList [BS.pack [1, 2], BS.pack [3, 4]]) C.$= conduitGet (getWord8 >> fail "omfg") C.$$ consume of
+    Left _ -> True
+    Right _ -> False))
+
+conduittest11 :: Test
+conduittest11 = TestCase (assertBool "Immediate failure works"
+  (case (sourceList [BS.pack [1, 2], BS.pack [3, 4]]) C.$= conduitGet (fail "omfg") C.$$ consume of
+    Left _ -> True
+    Right _ -> False))
+
+conduittest12 :: Test
+conduittest12 = TestCase (assertBool "Immediate failure with empty input works"
+  (case (sourceList []) C.$= conduitGet (fail "omfg") C.$$ consume of
+    Left _ -> True
+    Right _ -> False))
+
+sinktests = TestList [ sinktest1
+                     , sinktest2
+                     , sinktest3
+                     , sinktest4
+                     , sinktest5
+                     ]
+
+conduittests = TestList [ conduittest1
+                        , conduittest2
+                        , conduittest3
+                        , conduittest4
+                        , conduittest5
+                        , conduittest6
+                        , conduittest7
+                        , conduittest8
+                        , conduittest9
+                        , conduittest10
+                        , conduittest11
+                        , conduittest12
+                        ]
 
 hunittests = TestList [sinktests, conduittests]
 
-tests = hUnitTestToTests hunittests
+--tests = hUnitTestToTests hunittests
 
 main = do
   counts <- runTestTT hunittests
