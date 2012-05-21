5632db79f8bfb45f9549f5ea1c7b1cade41f09cb
Litherum <litherum@gmail.com>
2012-05-20 21:01:15 -0700

    Using Exceptions instead of Errors as the default implementation

--- Data/Conduit/Cereal.hs
+++ Data/Conduit/Cereal.hs
@@ -1,44 +1,44 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 -- | Turn a 'Get' into a 'Sink' and a 'Put' into a 'Source'
 -- These functions are built upno the Data.Conduit.Cereal.Internal functions with default
 -- implementations of 'ErrorHandler' and 'TerminationHandler'
 --
--- The default 'ErrorHandler' and 'TerminationHandler' both throw a 'GetError' with 'throwError'.
+-- The default 'ErrorHandler' and 'TerminationHandler' both throw a 'GetException'.
 
-module Data.Conduit.Cereal (GetError, sinkGet, conduitGet, sourcePut) where
+module Data.Conduit.Cereal (GetException, sinkGet, conduitGet, sourcePut) where
 
-import           Control.Monad.Error
+import           Control.Monad.Exception
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Conduit as C
 import           Data.Conduit.Cereal.Internal
 import           Data.Conduit.List (sourceList)
 import           Data.Serialize hiding (get, put)
+import           Data.Typeable
 
-data GetError = GetError String
-  deriving (Show, Eq)
+data GetException = GetException String
+  deriving (Show, Typeable)
 
-instance Error GetError where
-  noMsg = GetError ""
-  strMsg = GetError
+instance Exception GetException
 
 -- | Run a 'Get' repeatedly on the input stream, producing an output stream of whatever the 'Get' outputs.
-conduitGet :: MonadError GetError m => Get output -> C.Conduit BS.ByteString m output
+conduitGet :: MonadException m => Get output -> C.Conduit BS.ByteString m output
 conduitGet = mkConduitGet errorHandler 
-  where errorHandler msg _ = pipeError $ strMsg msg 
+  where errorHandler msg _ = pipeError $ GetException msg 
 
 -- | Convert a 'Get' into a 'Sink'. The 'Get' will be streamed bytes until it returns 'Done' or 'Fail'.
 --
 -- If 'Get' succeed it will return the data read and unconsumed part of the input stream.
 -- If the 'Get' fails due to deserialization error or early termination of the input stream it raise an error.
-sinkGet :: MonadError GetError m => Get r -> C.Sink BS.ByteString m r
+sinkGet :: MonadException m => Get r -> C.Sink BS.ByteString m r
 sinkGet = mkSinkGet errorHandler terminationHandler 
-  where errorHandler msg _ = pipeError $ strMsg msg 
-        terminationHandler f _ = let Fail msg = f BS.empty in pipeError $ strMsg msg 
+  where errorHandler msg _ = pipeError $ GetException msg 
+        terminationHandler f _ = let Fail msg = f BS.empty in pipeError $ GetException msg 
 
-pipeError :: MonadError e m => e -> C.Pipe i o m r
-pipeError e = C.PipeM (throwError e) undefined 
+pipeError :: (MonadException m, Exception e) => e -> C.Pipe i o m r
+pipeError e = C.PipeM (throw e) undefined 
 
 -- | Convert a 'Put' into a 'Source'. Runs in constant memory.
 sourcePut :: Monad m => Put -> C.Source m BS.ByteString
--- Test/CerealConduit.hs
+++ Test/CerealConduit.hs
@@ -3,7 +3,9 @@
 module Test.CerealConduit where
 
 import Control.Monad.Identity
+import Control.Monad.Exception
 import Control.Monad.Error
+import Control.Monad.Trans.Maybe
 import Test.HUnit
 import qualified Data.Conduit as C
 import Data.Conduit.Cereal
@@ -17,6 +19,10 @@ import Data.Word
 import qualified Data.List as L
 import Prelude hiding (take)
 
+-- For the sake of these tests, all SomeExceptions are equal
+instance Eq SomeException where
+  a == b = True
+
 twoItemGet :: Get Word8
 twoItemGet = do
   x <- getWord8
@@ -26,36 +32,36 @@ twoItemGet = do
 sinktest1 :: Test
 sinktest1 = TestCase (assertEqual "Handles starting with empty bytestring"
   (Right 1)
-  ((sourceList [BS.pack [], BS.pack [1]]) C.$$ (sinkGet getWord8)))
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [], BS.pack [1]]) C.$$ (sinkGet getWord8)))
 
 sinktest2 :: Test
 sinktest2 = TestCase (assertEqual "Handles empty bytestring in middle"
   (Right [1, 3])
-  ((sourceList [BS.pack [1], BS.pack [], BS.pack [3]]) C.$$ (sinkGet (do
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [1], BS.pack [], BS.pack [3]]) C.$$ (sinkGet (do
     x <- getWord8
     y <- getWord8
     return [x, y]))))
 
 sinktest3 :: Test
 sinktest3 = TestCase (assertBool "Handles no data"
-  (case (sourceList []) C.$$ (sinkGet getWord8) of
+  (case runIdentity $ runExceptionT $ (sourceList []) C.$$ (sinkGet getWord8) of
     Right _ -> False
     Left _ -> True))
 
 sinktest4 :: Test
 sinktest4 = TestCase (assertEqual "Consumes no data"
   (Right ())
-  ((sourceList [BS.pack [1]]) C.$$ (sinkGet $ return ())))
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [1]]) C.$$ (sinkGet $ return ())))
 
 sinktest5 :: Test
 sinktest5 = TestCase (assertEqual "Empty list"
   (Right ())
-  ((sourceList []) C.$$ (sinkGet $ return ())))
+  (runIdentity $ runExceptionT $ (sourceList []) C.$$ (sinkGet $ return ())))
 
 sinktest6 :: Test
 sinktest6 = TestCase (assertEqual "Leftover input works"
   (Right (1, BS.pack [2, 3, 4, 5]))
-  ((sourceList [BS.pack [1, 2, 3], BS.pack [4, 5]]) C.$$ (do
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [1, 2, 3], BS.pack [4, 5]]) C.$$ (do
     output <- sinkGet getWord8
     output' <- CL.consume
     return (output, BS.concat output'))))
@@ -81,63 +87,63 @@ sinktest7 = TestCase (assertBool "Leftover input with failure works"
 conduittest1 :: Test
 conduittest1 = TestCase (assertEqual "Handles starting with empty bytestring"
   (Right [])
-  ((sourceList [BS.pack [], BS.pack [1]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [], BS.pack [1]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
 
 conduittest2 :: Test
 conduittest2 = TestCase (assertEqual "Works when the get is split across items"
   (Right [3])
-  ((sourceList [BS.pack [1], BS.pack [2]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [1], BS.pack [2]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
 
 conduittest3 :: Test
 conduittest3 = TestCase (assertEqual "Works when empty bytestring in middle of get"
   (Right [3])
-  ((sourceList [BS.pack [1], BS.pack [], BS.pack [2]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [1], BS.pack [], BS.pack [2]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
 
 conduittest4 :: Test
 conduittest4 = TestCase (assertEqual "Works when empty bytestring at end of get"
   (Right [3])
-  ((sourceList [BS.pack [1, 2], BS.pack []]) C.$= conduitGet twoItemGet C.$$ CL.consume))
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [1, 2], BS.pack []]) C.$= conduitGet twoItemGet C.$$ CL.consume))
 
 conduittest5 :: Test
 conduittest5 = TestCase (assertEqual "Works when multiple gets are in an item"
   (Right [3, 7])
-  ((sourceList [BS.pack [1, 2, 3, 4]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [1, 2, 3, 4]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
 
 conduittest6 :: Test
 conduittest6 = TestCase (assertEqual "Works with leftovers"
   (Right [3])
-  ((sourceList [BS.pack [1, 2, 3]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [1, 2, 3]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
 
 conduittest7 :: Test
 conduittest7 = let c = 10 in TestCase (assertEqual "Works with infinite lists"
   (Right $ L.replicate c ())
-  ((sourceList [BS.pack [1, 2, 3]]) C.$= conduitGet (return ()) C.$$ take c))
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [1, 2, 3]]) C.$= conduitGet (return ()) C.$$ take c))
 
 conduittest8 :: Test
 conduittest8 = let c = 10 in TestCase (assertEqual "Works with empty source and infinite lists"
   (Right $ L.replicate c ())
-  ((sourceList []) C.$= conduitGet (return ()) C.$$ take c))
+  (runIdentity $ runExceptionT $ (sourceList []) C.$= conduitGet (return ()) C.$$ take c))
 
 conduittest9 :: Test
 conduittest9 = let c = 10 in TestCase (assertEqual "Works with two well-placed items"
   (Right [3, 7])
-  ((sourceList [BS.pack [1, 2], BS.pack [3, 4]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
+  (runIdentity $ runExceptionT $ (sourceList [BS.pack [1, 2], BS.pack [3, 4]]) C.$= conduitGet twoItemGet C.$$ CL.consume))
 
 conduittest10 :: Test
 conduittest10 = TestCase (assertBool "Failure works"
-  (case (sourceList [BS.pack [1, 2], BS.pack [3, 4]]) C.$= conduitGet (getWord8 >> fail "omfg") C.$$ CL.consume of
+  (case runIdentity $ runExceptionT $ (sourceList [BS.pack [1, 2], BS.pack [3, 4]]) C.$= conduitGet (getWord8 >> fail "omfg") C.$$ CL.consume of
     Left _ -> True
     Right _ -> False))
 
 conduittest11 :: Test
 conduittest11 = TestCase (assertBool "Immediate failure works"
-  (case (sourceList [BS.pack [1, 2], BS.pack [3, 4]]) C.$= conduitGet (fail "omfg") C.$$ CL.consume of
+  (case runIdentity $ runExceptionT $ (sourceList [BS.pack [1, 2], BS.pack [3, 4]]) C.$= conduitGet (fail "omfg") C.$$ CL.consume of
     Left _ -> True
     Right _ -> False))
 
 conduittest12 :: Test
 conduittest12 = TestCase (assertBool "Immediate failure with empty input works"
-  (case (sourceList []) C.$= conduitGet (fail "omfg") C.$$ CL.consume of
+  (case runIdentity $ runExceptionT $ (sourceList []) C.$= conduitGet (fail "omfg") C.$$ CL.consume of
     Left _ -> True
     Right _ -> False))
 
@@ -145,8 +151,8 @@ conduittest12 = TestCase (assertBool "Immediate failure with empty input works"
 -- This test CAN'T work because of the type of HaveOutput.
 conduittest13 :: Test
 conduittest13 = TestCase (assertEqual "Leftover success conduit input works"
-  (Right ([12], BS.pack [3, 4, 5]))
-  (runIdentity $ runErrorT $ (sourceList [BS.pack [10, 2, 3], BS.pack [4, 5]]) C.$$ (do
+  ([12], BS.pack [3, 4, 5])
+  (runIdentity $ (sourceList [BS.pack [10, 2, 3], BS.pack [4, 5]]) C.$$ (do
     output <- (conduitGet twoItemGet) C.=$ (CL.take 1)
     output' <- CL.consume
     return (output, BS.concat output'))))
@@ -155,7 +161,7 @@ conduittest13 = TestCase (assertEqual "Leftover success conduit input works"
 conduittest14 :: Test
 conduittest14 = TestCase (assertEqual "Leftover failure conduit input works"
   (Right ([], BS.singleton 1))
-  (runIdentity $ runErrorT $ (sourceList [BS.singleton 1]) C.$$ (do
+  (runIdentity $ runExceptionT $ (sourceList [BS.singleton 1]) C.$$ (do
     output <- (conduitGet twoItemGet) C.=$ (CL.take 1)
     output' <- CL.consume
     return (output, BS.concat output'))))
