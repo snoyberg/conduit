b3a7cc5176aacc3e45009124cb79738b65dfe414
Michael Snoyman <michael@snoyman.com>
2012-06-10 08:57:56 +0300

    Consolidated benchmarks

--- conduit/bench.hs
+++ conduit/bench.hs
@@ -14,19 +14,33 @@ import Control.Applicative
 import Data.Maybe
 import qualified "pipes-core" Control.Pipe as PC
 import qualified "pipes-core" Control.Pipe.Combinators as PCC
+import qualified "conduitfour" Data.Conduit as C4
+import qualified "conduitfour" Data.Conduit.List as CL4
+import qualified "conduitfour" Data.Conduit.Binary as CB4
+import qualified Data.ByteString as S
+import Control.Monad.Trans.Resource
+import Control.Monad.Trans.Class (lift)
+import qualified System.IO as SIO
+import qualified "pipes-core" Control.Pipe.Exception as PCE
 
 main :: IO ()
 main = defaultMain
-    [ bench "bigsum-resourcet-io" (whnfIO $ C.runResourceT $ CL.sourceList [1..1000 :: Int] C.$$ CL.fold (+) 0)
-    , bench "bigsum-io" (whnfIO $ CL.sourceList [1..1000 :: Int] C.$$ CL.fold (+) 0)
-    , bench "bigsum-st" $ whnf (\i -> (runST $ CL.sourceList [1..1000 :: Int] C.$$ CL.fold (+) i)) 0
-    , bench "bigsum-identity" $ whnf (\i -> (runIdentity $ CL.sourceList [1..1000 :: Int] C.$$ CL.fold (+) i)) 0
-    , bench "bigsum-spipe" $ whnf (\i -> (runIdentity $ (mapM_ CI.yield [1..1000 :: Int]) C.$$ CL.fold (+) i)) 0
-    , bench "bigsum-foldM" $ whnf (\i -> (runIdentity $ foldM (\a b -> return $! a + b) i [1..1000 :: Int])) 0
-    , bench "bigsum-pure" $ whnf (\i -> foldl' (+) i [1..1000 :: Int]) 0
-    , bench "bigsum-pipes" $ whnf pipeTest 0
-    , bench "bigsum-pipes-core" $ whnf pipeCoreTest 0
-    , bench "bigsum-pipes-core-seq" $ whnf pipeCoreTestSeq 0
+    [ bgroup "bigsum"
+        [ bench "conduit5" $ whnf (\i -> (runIdentity $ CL.sourceList [1..1000 :: Int] C.$$ CL.fold (+) i)) 0
+        , bench "conduit4" $ whnf (\i -> (runIdentity $ CL4.sourceList [1..1000 :: Int] C4.$$ CL4.fold (+) i)) 0
+        , bench "pipes" $ whnf pipeTestSum 0
+        , bench "pipes-core" $ whnf pipeCoreTest 0
+        , bench "pipes-core-seq" $ whnf pipeCoreTestSeq 0
+        , bench "pure" $ whnf (\i -> foldl' (+) i [1..1000 :: Int]) 0
+        ]
+    , bgroup "bytecount"
+        [ bench "conduit5" (whnfIO $ C.runResourceT $ CB.sourceFile "foo" C.$$ CL.fold (\x bs -> x + S.length bs) 0)
+        , bench "conduit4" (whnfIO $ C4.runResourceT $ CB4.sourceFile "foo" C4.$$ CL4.fold (\x bs -> x + S.length bs) 0)
+        , bench "pipes" (whnfIO pipeTestCount)
+        , bench "pipes-resource" (whnfIO pipeTestResource)
+        , bench "pipes-core" (whnfIO pipesCoreTest)
+        , bench "pipes-core-resource" (whnfIO pipesCoreTestResource)
+        ]
     ]
 
 pipeFold :: Monad m => (b -> a -> b) -> b -> Frame a o m b
@@ -46,8 +60,8 @@ pipeSourceList list = Frame $ do
     mapM_ yieldF list
     close $ return ()
 
-pipeTest :: Int -> Int
-pipeTest start = fromMaybe (-1) $ runIdentity $ runFrame $ (Just <$> pipeFold (+) start) <-< (Nothing <$ pipeSourceList [1..1000 :: Int])
+pipeTestSum :: Int -> Int
+pipeTestSum start = fromMaybe (-1) $ runIdentity $ runFrame $ (Just <$> pipeFold (+) start) <-< (Nothing <$ pipeSourceList [1..1000 :: Int])
 
 pipesCoreFold :: Monad m => (b -> a -> b) -> b -> PC.Pipe a x m b
 pipesCoreFold f = go
@@ -59,3 +73,65 @@ pipeCoreTest start = fromMaybe (-1) $ runIdentity $ PC.runPurePipe_ $ PCC.fromLi
 
 pipeCoreTestSeq :: Int -> Int
 pipeCoreTestSeq start = fromMaybe (-1) $ runIdentity $ PC.runPurePipe_ $ PCC.fromList [1..1000 :: Int] PCC.$$ pipesCoreFold (+) start
+
+sourceFilePipes :: FilePath -> Frame i S.ByteString IO Int
+sourceFilePipes file = Frame $ close $ do
+    h <- lift $ SIO.openBinaryFile file SIO.ReadMode
+    finallyP (SIO.hClose h) (pull h)
+  where
+    pull h = do
+        bs <- lift $ S.hGetSome h 4096
+        if S.null bs
+            then return 0
+            else do
+                yieldF bs
+                pull h
+
+sourceFilePipesResource :: FilePath -> Frame i S.ByteString (ResourceT IO) Int
+sourceFilePipesResource file = Frame $ close $ do
+    (key, h) <- lift $ allocate (SIO.openBinaryFile file SIO.ReadMode) SIO.hClose
+    finallyP (release key) (pull h)
+  where
+    pull h = do
+        bs <- lift $ lift $ S.hGetSome h 4096
+        if S.null bs
+            then return (-2)
+            else do
+                yieldF bs
+                pull h
+
+pipeTestCount = runFrame $ pipeFold (\x bs -> x + S.length bs) 0 <-< (sourceFilePipes "foo")
+pipeTestResource = runResourceT $ runFrame $ pipeFold (\x bs -> x + S.length bs) 0 <-< (sourceFilePipesResource "foo")
+
+sourceFilePipesCore :: FilePath -> PC.Pipe i S.ByteString IO Int
+sourceFilePipesCore file =
+    PCE.bracket
+        (SIO.openBinaryFile file SIO.ReadMode)
+        SIO.hClose
+        pull
+  where
+    pull h = do
+        bs <- lift $ S.hGetSome h 4096
+        if S.null bs
+            then return 0
+            else do
+                PC.yield bs
+                pull h
+
+sourceFilePipesCoreResource :: FilePath -> PC.Pipe i S.ByteString (ResourceT IO) Int
+sourceFilePipesCoreResource file =
+    PCE.bracket
+        (allocate (SIO.openBinaryFile file SIO.ReadMode) SIO.hClose)
+        (release . fst)
+        (pull . snd)
+  where
+    pull h = do
+        bs <- lift $ lift $ S.hGetSome h 4096
+        if S.null bs
+            then return 0
+            else do
+                PC.yield bs
+                pull h
+
+pipesCoreTest = PC.runPipe $ sourceFilePipesCore "foo" PCC.$$ PCC.fold (\x bs -> x + S.length bs) 0
+pipesCoreTestResource = runResourceT $ PC.runPurePipe_ $ sourceFilePipesCoreResource "foo" PCC.$$ PCC.fold (\x bs -> x + S.length bs) 0
