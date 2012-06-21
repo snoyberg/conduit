52d7bc0b551b877de92be4c87f933e3ffb1bb9f6
Michael Snoyman <michael@snoyman.com>
2012-06-21 09:24:46 +0300

    Better codeWith

--- base64-conduit/Data/Conduit/Base64.hs
+++ base64-conduit/Data/Conduit/Base64.hs
@@ -29,24 +29,29 @@ decodeURL = codeWith 4 B64U.decodeLenient
 
 codeWith :: Monad m => Int -> (ByteString -> ByteString) -> GInfConduit ByteString m ByteString
 codeWith size f =
-    go S.empty
+    loop
   where
-    go bs = awaitE >>= either (close bs) (push bs)
+    loop = awaitE >>= either return push
 
-    close bs r = do
-        unless (S.null bs) $ yield $ f bs
-        return r
+    loopWith bs
+        | S.null bs = loop
+        | otherwise = awaitE >>= either (\r -> yield (f bs) >> return r) (pushWith bs)
 
-    push bs1 bs2 | S.length bs1 + S.length bs2 < size = go (S.append bs1 bs2)
-    push bs1 bs2 = assert (S.length bs1 < size) $ do
-        bs <-
-            case S.length bs1 `mod` size of
-                0 -> return bs2
-                m -> do
-                    let (x, y) = S.splitAt (size - m) bs2
-                        bs1' = S.append bs1 x
-                    assert (S.length bs1' `mod` size == 0) $ yield $ f bs1'
-                    return y
-        let (x, y) = S.splitAt (size * (S.length bs `div` size)) bs
+    push bs = do
+        let (x, y) = S.splitAt (len - (len `mod` size)) bs
         unless (S.null x) $ yield $ f x
-        go y
+        loopWith y
+      where
+        len = S.length bs
+
+    pushWith bs1 bs2 | S.length bs1 + S.length bs2 < size = loopWith (S.append bs1 bs2)
+    pushWith bs1 bs2 = assertion1 $ assertion2 $ do
+        yield $ f bs1'
+        push y
+      where
+        m = S.length bs1 `mod` size
+        (x, y) = S.splitAt (size - m) bs2
+        bs1' = S.append bs1 x
+
+        assertion1 = assert $ S.length bs1 < size
+        assertion2 = assert $ S.length bs1' `mod` size == 0
