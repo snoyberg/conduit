229efd15548cd7d6af040128d98c8716b5cc43f2
Nicolas Trangez <ikke@nicolast.be>
2012-08-29 12:20:32 +0200

    Add finalizers to udpflow example
    Yet, according to strace, they don't seem to work...

--- network-conduit/udpflow.hs
+++ network-conduit/udpflow.hs
@@ -7,9 +7,8 @@ import Data.Conduit.Network.UDP
 import qualified Data.Conduit.List as CL
 
 import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (forever)
 import Data.ByteString.Char8 ()
-import Network.Socket (addrAddress)
+import Network.Socket (addrAddress, sClose)
 
 localhost :: String
 localhost = "127.0.0.1"
@@ -17,18 +16,26 @@ port :: Int
 port = 4000
 
 receiver :: IO ()
-receiver = do
-    sock <- bindPort port (Host localhost)
-    sourceSocket sock 4096 $$ CL.mapM_ (\_ -> return ())
+receiver = runResourceT $ src $$ CL.mapM_ (\_ -> return ())
+  where
+    src = bracketP
+            (bindPort port (Host localhost))
+            sClose
+            (\sock -> sourceSocket sock 4096)
 
 sender :: IO ()
 sender = do
     (sock, addr) <- getSocket localhost port
-    CL.sourceList (repeat ("abc", addrAddress addr)) $$ sinkSocket sock
+
+    let sink = addCleanup
+                (const $ sClose sock)
+                (sinkSocket sock)
+
+    CL.sourceList (repeat ("abc", addrAddress addr)) $$ sink
 
 main :: IO ()
 main = do
     forkIO receiver
     forkIO sender
 
-    forever $ threadDelay $ 1000 * 1000 * 10
+    threadDelay $ 1000 * 1000 * 5
