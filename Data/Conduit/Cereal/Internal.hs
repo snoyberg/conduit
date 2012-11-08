697494ae8a9f47e6f3a9daa0d5a38a0a833d6a29
litherum <litherum@gmail.com>
2012-11-07 21:15:32 -0800

    Removing traces

--- Data/Conduit/Cereal/Internal.hs
+++ Data/Conduit/Cereal/Internal.hs
@@ -13,7 +13,6 @@ import           Control.Monad (when)
 import qualified Data.ByteString as BS
 import qualified Data.Conduit as C
 import           Data.Serialize hiding (get, put)
-import Debug.Trace
 
 -- | What should we do if the Get fails?
 type ConduitErrorHandler m o = String -> C.GLConduit BS.ByteString m o
@@ -29,19 +28,19 @@ mkConduitGet :: C.MonadThrow m
              -> C.GLConduit BS.ByteString m o
 mkConduitGet errorHandler get = consume True (runGetPartial get) [] BS.empty
   where pull f b s
-          | BS.null s = trace "s null" $ C.await >>= \ x -> case x of
-                          Nothing -> trace (show b) $ when (not $ null b) (C.leftover $ BS.concat $ reverse b)
-                          Just a -> trace ("pulled " ++ (show a)) $ pull f b a
-          | otherwise = trace ("pulled' " ++ (show s)) $ consume False f b s
+          | BS.null s = C.await >>= \ x -> case x of
+                          Nothing -> when (not $ null b) (C.leftover $ BS.concat $ reverse b)
+                          Just a -> pull f b a
+          | otherwise = consume False f b s
         consume initial f b s = case f s of
           Fail msg  -> do
             when (not $ null b) (C.leftover $ BS.concat $ reverse consumed)
             errorHandler msg
-          Partial p -> trace "Partial" $ pull p consumed BS.empty
+          Partial p -> pull p consumed BS.empty
           Done a s' -> case initial of
                          -- this only works because the Get will either _always_ consume no input, or _never_ consume no input.
                          True  -> sequence_ $ repeat $ C.yield a
-                         False -> trace "done" $ C.yield a >> trace "after done" (pull (runGetPartial get) [] s')
+                         False -> C.yield a >> pull (runGetPartial get) [] s'
           where consumed = s : b
 
 -- | Construct a sinkGet with the specified 'ErrorHandler' and 'TerminationHandler'
