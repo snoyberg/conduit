9f5ff4c80d49fa571d28c979c7669b5d127c26ad
Michael Snoyman <michael@snoyman.com>
2013-03-01 08:30:27 +0200

    Fix doctests

--- conduit/test/doctests.hs
+++ conduit/test/doctests.hs
@@ -1,11 +1,6 @@
 module Main where
 
-import System.Directory
 import Test.DocTest
 
 main :: IO ()
-main = do
-  writeFile "input.txt" "some dummy content"
-  doctest ["Data/Conduit.hs"]
-  removeFile "input.txt"
-  removeFile "output.txt"
+main = doctest ["Data/Conduit.hs"]
