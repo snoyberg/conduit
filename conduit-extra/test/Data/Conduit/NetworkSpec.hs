20ad20de4b39929e343684a80a31446c7138fa1b
Michael Snoyman <michael@snoyman.com>
2014-03-21 15:18:37 +0200

    Include network-conduit in conduit-extra

--- conduit-extra/test/Data/Conduit/NetworkSpec.hs
+++ conduit-extra/test/Data/Conduit/NetworkSpec.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
+module Data.Conduit.NetworkSpec (spec) where
+
 import Data.Conduit
 import Data.Conduit.Network
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad (replicateM_)
+import Test.Hspec
 
-main :: IO ()
-main = do
+spec :: Spec
+spec = it "Data.Conduit.Network" $ do
     _ <- forkIO $ runTCPServer (serverSettings 4009 "*4") echo
     threadDelay 1000000
     replicateM_ 10000
