58907c5fca38a88a2aa21f1771570990003e8387
Michael Snoyman <michael@snoyman.com>
2014-08-10 15:54:14 +0300

    Add unrolled conduit benchmarks

--- conduit/benchmarks/sum.hs
+++ conduit/benchmarks/sum.hs
@@ -1,5 +1,6 @@
 import Criterion.Main
 import Data.Conduit
+import Data.Conduit.Internal (ConduitM (..), Pipe (..))
 import qualified Data.Conduit.List as CL
 import Data.List (foldl')
 import Control.Monad (foldM)
@@ -9,13 +10,13 @@ import Data.Functor.Identity (runIdentity)
 upper :: Int
 upper = 10000
 
-plusM :: Monad m => Int -> Int -> m Int
+plusM :: (Num a, Monad m) => a -> a -> m a
 plusM x y = return $! x + y
 
 main :: IO ()
 main = do
     upperRef <- newIORef upper
-    defaultMain
+    defaultMain $ reverse
         [ bench "foldl'" $ flip whnf upper $ \upper' ->
             foldl' (+) 0 [1..upper']
         , bench "foldM" $ whnfIO $ do
@@ -26,4 +27,37 @@ main = do
         , bench "conduit IO" $ whnfIO $ do
             upper' <- readIORef upperRef
             CL.enumFromTo 1 upper' $$ CL.foldM plusM 0
+        , bench "conduit pure unrolled" $ whnfIO $ do
+            upper' <- readIORef upperRef
+            CL.enumFromTo 1 upper' $$ sumC
+        , bench "conduit IO unrolled" $ whnfIO $ do
+            upper' <- readIORef upperRef
+            CL.enumFromTo 1 upper' $$ sumMC
         ]
+
+sumC :: (Num a, Monad m) => Consumer a m a
+sumC =
+    ConduitM (go 0)
+  where
+    go total =
+        NeedInput next done
+      where
+        done () = Done total
+
+        next i =
+            total' `seq` go total'
+          where
+            total' = total + i
+
+sumMC :: (Num a, Monad m) => Consumer a m a
+sumMC =
+    ConduitM (go 0)
+  where
+    go total =
+        NeedInput next done
+      where
+        done () = Done total
+
+        next i = PipeM $ do
+            total' <- plusM total i
+            total' `seq` return (go total')
