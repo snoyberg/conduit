4b0385dfcc2f1144c646f48083ab2ab262de5c92
Michael Snoyman <michael@snoyman.com>
2014-08-20 14:23:34 +0300

    Stream fusion for sliding window/vector benchmark

--- conduit/benchmarks/optimize-201408.hs
+++ conduit/benchmarks/optimize-201408.hs
@@ -191,6 +191,9 @@ slidingWindow window = do
         [ TBIOBench "low level, Seq" expected
             (swLowLevelSeq window upperRef id (\x y -> x . (F.toList y:)) ($ []))
             (swLowLevelSeq window upperRef () (\() y -> rnf y) id)
+        , TBIOBench "conduit, Seq" expected
+            (swConduitSeq window upperRef id (\x y -> x . (F.toList y:)) ($ []))
+            (swConduitSeq window upperRef () (\() y -> rnf y) id)
         {- https://ghc.haskell.org/trac/ghc/ticket/9446
         , TBIOBench "low level, boxed Vector" expected
             (swLowLevelVector window upperRef id (\x y -> x . (VB.toList y:)) ($ []))
@@ -198,12 +201,6 @@ slidingWindow window = do
             -}
         , TBBench $ bench "low level, boxed Vector" $ whnfIO $
             swLowLevelVector window upperRef () (\() y -> rnf (y :: VB.Vector Int)) id
-        , TBIOBench "low level, unboxed Vector" expected
-            (swLowLevelVector window upperRef id (\x y -> x . (VU.toList y:)) ($ []))
-            (swLowLevelVector window upperRef () (\() y -> rnf (y :: VU.Vector Int)) id)
-        , TBIOBench "conduit, Seq" expected
-            (swConduitSeq window upperRef id (\x y -> x . (F.toList y:)) ($ []))
-            (swConduitSeq window upperRef () (\() y -> rnf y) id)
 
         {- https://ghc.haskell.org/trac/ghc/ticket/9446
         , TBIOBench "conduit, boxed Vector" expected
@@ -214,6 +211,10 @@ slidingWindow window = do
         , TBBench $ bench "conduit, boxed Vector" $ whnfIO $
             swConduitVector window upperRef () (\() y -> rnf (y :: VB.Vector Int)) id
 
+
+        , TBIOBench "low level, unboxed Vector" expected
+            (swLowLevelVector window upperRef id (\x y -> x . (VU.toList y:)) ($ []))
+            (swLowLevelVector window upperRef () (\() y -> rnf (y :: VU.Vector Int)) id)
         , TBIOBench "conduit, unboxed Vector" expected
             (swConduitVector window upperRef id (\x y -> x . (VU.toList y:)) ($ []))
             (swConduitVector window upperRef () (\() y -> rnf (y :: VU.Vector Int)) id)
@@ -309,7 +310,14 @@ swConduitVector window upperRef t0 f final = do
     return $! final t
 
 slidingWindowC :: Monad m => Int -> Conduit a m (Seq.Seq a)
-slidingWindowC sz =
+slidingWindowC = slidingWindowCC
+{-# INLINE [0] slidingWindowC #-}
+{-# RULES "unstream slidingWindowC"
+    forall i. slidingWindowC i = CI.unstream (CI.StreamConduit (slidingWindowCC i) (slidingWindowS i))
+  #-}
+
+slidingWindowCC :: Monad m => Int -> Conduit a m (Seq.Seq a)
+slidingWindowCC sz =
     go sz mempty
   where
     goContinue st = await >>=
@@ -324,9 +332,38 @@ slidingWindowC sz =
                  Nothing | n < sz -> yield st
                          | otherwise -> return ()
                  Just x -> go (n-1) (st Seq.|> x)
+{-# INLINE slidingWindowCC #-}
+
+slidingWindowS :: Monad m => Int -> CI.Stream m a () -> CI.Stream m (Seq.Seq a) ()
+slidingWindowS sz (CI.Stream step ms0) =
+    CI.Stream step' $ liftM (\s -> Left (s, sz, mempty)) ms0
+  where
+    step' (Left (s, 0, st)) = return $ CI.Emit (Right (s, st)) st
+    step' (Left (s, i, st)) = do
+        res <- step s
+        return $ case res of
+            CI.Stop () -> CI.Stop ()
+            CI.Skip s' -> CI.Skip $ Left (s', i, st)
+            CI.Emit s' a -> CI.Skip $ Left (s', i - 1, st Seq.|> a)
+    step' (Right (s, st)) = do
+        res <- step s
+        return $ case res of
+            CI.Stop () -> CI.Stop ()
+            CI.Skip s' -> CI.Skip $ Right (s', st)
+            CI.Emit s' a ->
+                let st' = Seq.drop 1 st Seq.|> a
+                 in CI.Emit (Right (s', st')) st'
+{-# INLINE slidingWindowS #-}
 
 slidingVectorC :: V.Vector v a => Int -> Conduit a IO (v a)
-slidingVectorC sz = do
+slidingVectorC = slidingVectorCC
+{-# INLINE [0] slidingVectorC #-}
+{-# RULES "unstream slidingVectorC"
+    forall i. slidingVectorC i = CI.unstream (CI.StreamConduit (slidingVectorCC i) (slidingVectorS i))
+  #-}
+
+slidingVectorCC :: V.Vector v a => Int -> Conduit a IO (v a)
+slidingVectorCC sz = do
     mv <- newBuf
     mv2 <- newBuf
     go 0 mv mv2
@@ -350,3 +387,41 @@ slidingVectorC sz = do
             v <- liftIO $ V.unsafeFreeze $ VM.unsafeSlice (end' - sz) sz mv
             yield v
           go end' mv mv2
+
+slidingVectorS :: V.Vector v a => Int -> CI.Stream IO a () -> CI.Stream IO (v a) ()
+slidingVectorS sz (CI.Stream step ms0) =
+    CI.Stream step' ms1
+  where
+    bufSz = 2 * sz
+    newBuf = liftIO (VM.new bufSz)
+
+    ms1 = do
+        s <- ms0
+        mv <- newBuf
+        mv2 <- newBuf
+        return (s, 0, mv, mv2)
+
+    step' (_, -1, _, _) = return $ CI.Stop ()
+    step' (s, end, _mv, mv2) | end == bufSz = do
+        mv3 <- newBuf
+        return $ CI.Skip (s, sz, mv2, mv3)
+    step' (s, end, mv, mv2) = do
+        res <- step s
+        case res of
+            CI.Stop ()
+                | end > 0 && end < sz -> do
+                    v <- liftIO $ V.unsafeFreeze $ VM.take end mv
+                    return $ CI.Emit (s, -1, mv, mv2) v
+                | otherwise -> return $ CI.Stop ()
+            CI.Skip s' -> return $ CI.Skip (s', end, mv, mv2)
+            CI.Emit s' x -> liftIO $ do
+                VM.unsafeWrite mv end x
+                when (end > sz) $ VM.unsafeWrite mv2 (end - sz) x
+                let end' = end + 1
+                    state = (s', end', mv, mv2)
+                if end' >= sz
+                    then do
+                        v <- V.unsafeFreeze $ VM.unsafeSlice (end' - sz) sz mv
+                        return $ CI.Emit state v
+                    else return $ CI.Skip state
+{-# INLINE slidingVectorS #-}
