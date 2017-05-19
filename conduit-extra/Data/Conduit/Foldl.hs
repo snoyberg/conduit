2c22e0f67907d96bbc49891595e4fd7de799eabd
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-05-19 16:12:33 +0200

    Add adapter module for the foldl package to conduit-extra
    fixes #311

---
+++ conduit-extra/Data/Conduit/Foldl.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE RankNTypes #-}
+-- | Adapter module to work with the <https://hackage.haskell.org/package/foldl foldl> package.
+--
+-- @since 1.1.16
+module Data.Conduit.Foldl where
+
+import Data.Conduit
+import Control.Monad.Trans.Class (lift)
+import qualified Data.Conduit.List as CL
+
+-- | Convert a left fold into a 'Consumer'. This function is intended
+-- to be used with @purely@ from the
+-- <https://hackage.haskell.org/package/foldl foldl> package.
+--
+-- @since 1.1.16
+sinkFold :: Monad m => (x -> a -> x) -> x -> (x -> b) -> Consumer a m b
+sinkFold combine seed extract = fmap extract (CL.fold combine seed)
+
+-- | Convert a monadic left fold into a 'Consumer'. This function is
+-- intended to be used with @impurely@ from the
+-- <https://hackage.haskell.org/package/foldl foldl> package.
+--
+-- @since 1.1.16
+sinkFoldM :: Monad m => (x -> a -> m x) -> m x -> (x -> m b) -> Consumer a m b
+sinkFoldM combine seed extract =
+  lift . extract =<< CL.foldM combine =<< lift seed
