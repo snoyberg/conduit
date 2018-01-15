887fb50ea94b588859f7ac0394857837774d0322
Michael Snoyman <michael@snoyman.com>
2018-01-15 10:37:48 +0200

    Get a benchmark to compile again

--- conduit-extra/bench/blaze.hs
+++ conduit-extra/bench/blaze.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Data.Conduit
 import qualified Data.Conduit.List as CL
-import Data.Conduit.Blaze
+import Data.Conduit.ByteString.Builder
 import Criterion.Main
-import Blaze.ByteString.Builder
 import Data.Monoid
-import qualified Data.ByteString.Builder as BS
+import Data.ByteString.Builder
 import Data.Functor.Identity (runIdentity)
 import Control.Monad.ST (runST)
 import Data.ByteString.Lazy.Internal (defaultChunkSize)
@@ -14,7 +13,7 @@ count :: Int
 count = 100000
 
 single :: Builder
-single = copyByteString "Hello World!\n"
+single = shortByteString "Hello World!\n"
 
 oneBuilderLeft :: Builder
 oneBuilderLeft =
@@ -33,40 +32,37 @@ oneBuilderRight =
 builderSource :: Monad m => Source m Builder
 builderSource = CL.replicate count single
 
-singleBS :: BS.Builder
-singleBS = BS.shortByteString "Hello World!\n"
-
-oneBSBuilderLeft :: BS.Builder
+oneBSBuilderLeft :: Builder
 oneBSBuilderLeft =
     loop count mempty
   where
     loop 0 b = b
-    loop i b = loop (i - 1) (b <> singleBS)
+    loop i b = loop (i - 1) (b <> single)
 
-oneBSBuilderRight :: BS.Builder
+oneBSBuilderRight :: Builder
 oneBSBuilderRight =
     loop count mempty
   where
     loop 0 b = b
-    loop i b = loop (i - 1) (b <> singleBS)
+    loop i b = loop (i - 1) (b <> single)
 
-builderBSSource :: Monad m => Source m BS.Builder
-builderBSSource = CL.replicate count singleBS
+builderBSSource :: Monad m => ConduitT i Builder m ()
+builderBSSource = CL.replicate count single
 
 main :: IO ()
 main = defaultMain
     [ bench "conduit, strict, safe" $ whnfIO $
         builderSource $$ builderToByteString =$ CL.sinkNull
     , bench "conduit, strict, unsafe" $ whnfIO $
-        builderSource $$ unsafeBuilderToByteString (allocBuffer defaultChunkSize) =$ CL.sinkNull
+        builderSource $$ unsafeBuilderToByteString =$ CL.sinkNull
 
     , bench "one builder, left" $ nf toLazyByteString oneBuilderLeft
     , bench "one builder, right" $ nf toLazyByteString oneBuilderRight
     , bench "conduit, lazy" $ flip nf builderSource $ \src ->
         toLazyByteString $ runIdentity $ src $$ CL.fold (<>) mempty
 
-    , bench "one bs builder, left" $ nf BS.toLazyByteString oneBSBuilderLeft
-    , bench "one bs builder, right" $ nf BS.toLazyByteString oneBSBuilderRight
+    , bench "one bs builder, left" $ nf toLazyByteString oneBSBuilderLeft
+    , bench "one bs builder, right" $ nf toLazyByteString oneBSBuilderRight
     , bench "conduit BS, lazy" $ flip nf builderBSSource $ \src ->
-        BS.toLazyByteString $ runIdentity $ src $$ CL.fold (<>) mempty
+        toLazyByteString $ runIdentity $ src $$ CL.fold (<>) mempty
     ]
